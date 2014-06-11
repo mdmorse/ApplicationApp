da57373a00ad7fd3aa5a7c26e02ebc5370413898
Matt Morse <mmorse@comcast.net>
2014-06-11 10:39:30 -0600

    google

--- config/passport.js
+++ config/passport.js
@@ -38,8 +38,8 @@ var facebookStrategy= new FacebookStrategy({
 passport.use(facebookStrategy);
 
 passport.use(new GoogleStrategy({
-    consumerKey: 682333433362-jdatul4o4orn5adiiit590p1ncf27va2.apps.googleusercontent.com,
-    consumerSecret: bY4eoyQxT-QulXNfzk1EMTon,
+    consumerKey: '682333433362-jdatul4o4orn5adiiit590p1ncf27va2.apps.googleusercontent.com',
+    consumerSecret: 'bY4eoyQxT-QulXNfzk1EMTon',
     callbackURL: "http://fast-inlet-3968.herokuapp.com/oauth2callback"
   },
   function(token, tokenSecret, profile, done) {
